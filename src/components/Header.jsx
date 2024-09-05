b68e9bf99df4201a8e2943bf0fe6028bfc8516cc
anish <anishpravitha@gmail.com>
2024-09-05 09:28:03 +0530

    completed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,15 +7,15 @@ import { Link } from 'react-router-dom';
 function Header() {
   return (
     <>
-         <Navbar expand="sm" className="bg-secondary">
+         <Navbar expand="sm" className="bg-info">
       <Container>
-        <Navbar.Brand ><Link style={{textDecoration:'none', color:'white'}} to={'/'}>KKTM Govt. College, Pullut</Link></Navbar.Brand>
+        <Navbar.Brand ><Link style={{textDecoration:'none', color:'blue'}} to={'/'}>KKTM Govt. College, Pullut</Link></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />        
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link><Link style={{textDecoration:'none', color:'white'}} to={'/data'}>Data</Link></Nav.Link>
-            <Nav.Link><Link style={{textDecoration:'none', color:'white'}} to={'/tc'}>TC</Link></Nav.Link>
-            <Nav.Link><Link style={{textDecoration:'none', color:'white'}} to={'/display'}>Students</Link></Nav.Link>
+            <Nav.Link><Link style={{textDecoration:'none', color:'blue'}} to={'/data'}>Data</Link></Nav.Link>
+            <Nav.Link><Link style={{textDecoration:'none', color:'blue'}} to={'/tc'}>TC</Link></Nav.Link>
+            <Nav.Link><Link style={{textDecoration:'none', color:'blue'}} to={'/display'}>Students</Link></Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
